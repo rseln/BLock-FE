bee21fbd9a1c21a85b16b34c2700395c80c45cae
SeanLeBon <sean.chen0818@gmail.com>
2024-02-07 21:19:15 -0500

    fixed logout button in menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,11 +3,21 @@ import { bool } from 'prop-types';
 import { StyledMenu } from './MenuStyled';
 import CloseIcon from '@mui/icons-material/Close';
 import IconButton from '@mui/material/IconButton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Menu = ({ open, setOpen, ...props }) => {
   
   const isHidden = open ? true : false;
   const tabIndex = isHidden ? 0 : -1;
+  const {logout} = useAuth0();
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      }
+    })
+  }
 
   return (
     <StyledMenu open={open} aria-hidden={!isHidden} {...props}>
@@ -32,7 +42,7 @@ const Menu = ({ open, setOpen, ...props }) => {
         <span aria-hidden="true">💸</span>
         Book Here
         </a>
-      <a href="/welcome" tabIndex={tabIndex}>
+      <a href="/" tabIndex={tabIndex} onClick={handleLogout}>
         <span aria-hidden="true">📩</span>
         Logout
         </a>
